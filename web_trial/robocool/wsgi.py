68546a147cee043b5a6c12a0375493fff77dc3e1
root <root@ip-172-31-27-168.eu-west-3.compute.internal>
2020-09-09 10:52:13 +0000

    gunicorn

--- web_trial/robocool/wsgi.py
+++ web_trial/robocool/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/srv/web_trial')
+sys.path.append('/srv/web_trial/robocool')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'robocool.settings')
 
 application = get_wsgi_application()
