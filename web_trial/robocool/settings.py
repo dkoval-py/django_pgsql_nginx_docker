8247045e21bee13a083010fe56662a95065f2fcb
dkoval-py <61237783+dkoval-py@users.noreply.github.com>
2020-10-24 13:23:48 +0300

    Update settings.py
    change s3 region

--- web_trial/robocool/settings.py
+++ web_trial/robocool/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
 AWS_S3_FILE_OVERWRITE = False
 AWS_ACCESS_KEY_ID = 'Key'
 AWS_SECRET_ACCESS_KEY = 'PrivateKey'
-AWS_S3_REGION_NAME = 'eu-west-3'
+AWS_S3_REGION_NAME = 'eu-central-1'
 AWS_STORAGE_BUCKET_NAME = 'koval.crm.django'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 '''
